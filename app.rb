54a27aa169400578ff184ecef3515630ecda0041
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-07-07 12:01:39 +0300

    added a route to be called

--- app.rb
+++ app.rb
@@ -1,2 +1,10 @@
 require('sinatra')
 require('sinatra/reloader')
+also_reload('lib/**/*.rb')
+require('./lib/word')
+require('.lib/defination')
+
+get('/') do
+  @words = Word.all()
+  erb(index)
+end
