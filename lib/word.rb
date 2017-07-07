58dd55005f9502bc944fd112571d2ba2f06de322
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-07-07 11:22:50 +0300

    added a singleton class to find entered words

--- lib/word.rb
+++ lib/word.rb
@@ -32,3 +32,13 @@ end
   define_singleton_method(:clear) do
         @@words = []
       end
+
+      define_singleton_method(:find) do |id|
+        found_word = nil
+        @@words.each() do |word|
+          if word.id().eql?(id)
+            found_word = word
+          end
+        end
+        found_word
+      end
