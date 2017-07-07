d21b910b94c9aa6c12ee46f5093a9ab3aa6d57c4
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-07-07 11:33:33 +0300

    created a method for word

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -1,4 +1,18 @@
 require('rspec')
   require('word')
 
-  
+
+  describe('Word') do
+    before() do
+      Word.clear()
+    end
+
+    describe("#word") do
+      it("returns the word entered") do
+        test_word = Word.new("Machine Language")
+        expect(test_word.make()).to(eq("Machine Language"))
+      end
+    end
+
+
+  end
