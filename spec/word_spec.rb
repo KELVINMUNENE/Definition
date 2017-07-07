7a20b159c08b32f1a08769a81dbebeea0bde1f39
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-07-07 11:37:44 +0300

    created a method for id

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -10,9 +10,27 @@ require('rspec')
     describe("#word") do
       it("returns the word entered") do
         test_word = Word.new("Machine Language")
-        expect(test_word.make()).to(eq("Machine Language"))
+        expect(test_word.word()).to(eq("Machine Language"))
       end
     end
+
+    describe("#description") do
+      it("returns the description entered") do
+        test_word = Word.new("A language for the machine in computers")
+        expect(test_word.description()).to(eq("Machine Language"))
+      end
+    end
+
+
+
+    describe("#id") do
+      it("returns the id entered") do
+        test_word = Word.new("300")
+        expect(test_word.id()).to(eq("300"))
+      end
+    end
+
+
 
 
   end
