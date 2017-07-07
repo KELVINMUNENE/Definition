aa4cafab5bb05fd3c4e7e5bb7e01c59143b80102
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-07-07 12:36:50 +0300

    assigned attribute readers to create methods automatically

--- app.rb
+++ app.rb
@@ -2,9 +2,27 @@ require('sinatra')
 require('sinatra/reloader')
 also_reload('lib/**/*.rb')
 require('./lib/word')
-
+require('./lib/definition')
 
 get('/') do
   @words = Word.all()
-  erb(index)
+  erb(:index)
+end
+
+get('/word_form') do
+  erb(:word_form)
+end
+
+get('/word/:id') do
+  @word = Word.find(params.fetch('id').to_i())
+  erb(:definition)
+end
+
+post('/word') do
+  @word = params.fetch("word")
+  @definition = params.fetch("definition")
+  Definition.new({:definition => @definition})
+  Word.new({:word => @word}).save()
+  @words = Word.all()
+  erb(:word)
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -1,80 +1,58 @@
 require('rspec')
-  require('word')
+require('word.rb')
 
+describe(Word) do
 
-  describe('Word') do
-    before() do
-      Words.clear()
+  before() do
+   Word.clear()
+ end
+
+  describe('#word') do
+    it('returns a word') do
+      test_word = Word.new({:word => 'permanently'})
+      expect(test_word.word()).to(eq('permanently'))
     end
-
-    describe("#word") do
-      it("returns the word entered") do
-        test_word = Word.new("Machine Language")
-        expect(test_word.word()).to(eq("Machine Language"))
-      end
-    end
-
-    describe("#description") do
-      it("returns the description entered") do
-        test_word = Word.new("A language for the machine in computers")
-        expect(test_word.description()).to(eq("Machine Language"))
-      end
-    end
-
-
-
-    describe("#id") do
-      it("returns the id entered") do
-        test_word = Word.new("300")
-        expect(test_word.id()).to(eq("300"))
-      end
-    end
-
-    describe(".all") do
-          it("is empty at first") do
-            expect(Word.all()).to(eq([]))
-          end
-        end
-
-
-        describe("#save") do
-             it("adds a word to the array of saved words") do
-               test_word = Word.new("Machine Language")
-               test_word.save()
-               expect(Word.all()).to(eq([test_word]))
-             end
-           end
-
-
-
-           describe(".clear") do
-                 it("empties out all of the saved words") do
-                   Word.new("Machine Language").save()
-                   Word.clear()
-                   expect(Word.all()).to(eq([]))
-                 end
-               end
-
-
-
-
-               describe(".find") do
-                   it("returns a word by its id number") do
-                     test_word = Word.new()
-                     test_word.save()
-                     test_word2 = Word.new()
-                     test_word2.save()
-                     expect(Word.find(test_word.id())).to(eq(test_word))
-                   end
-                 end
-
-
-
-
-
-
-
-
-
-
   end
+
+  describe('#save') do
+   it('adds a word to the array of saved words') do
+     test_word = Word.new({:word => "forever"})
+     test_word.save()
+     expect(Word.all()).to(eq([test_word]))
+   end
+ end
+
+ describe('.all') do
+  it('is empty at first') do
+    expect(Word.all()).to(eq([]))
+  end
+end
+
+  describe('.clear') do
+    it('empties out all of the saved words') do
+    test_word = Word.new({:word => "forever"}).save()
+      Word.clear
+      expect(Word.all()).to(eq([]))
+    end
+  end
+
+  describe('#add_definition') do
+    it('adds a new definition to word') do
+      test_word = Word.new({:word => "permanently"})
+      test_definition = Definition.new({:definition => 'in a way that lasts or remains unchanged indefinitely; for all time.'})
+      test_word.add_definition(test_definition)
+      expect(test_word.definition()).to(eq([test_definition]))
+    end
+  end
+
+  describe(".find") do
+    it("returns a word by its id number") do
+      test_word = Word.new({:word => "forever"})
+      test_word.save()
+      test_word2 = Word.new({:word =>"permanently"})
+      test_word2.save()
+      expect(Word.find(test_word.id())).to(eq(test_word))
+    end
+  end
+
+end
